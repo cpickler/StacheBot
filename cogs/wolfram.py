0d5a7b96640919bc3350e9fb1c9512a4221b4de2
cpickler <christian.pickler@gmail.com>
2017-04-09 04:19:36 -0700

    Added necessary files for Heroku

--- cogs/wolfram.py
+++ cogs/wolfram.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os, aiohttp
+import urllib
 
 def setup(bot):
     bot.add_cog(WolframAlpha(bot))
@@ -12,7 +13,7 @@ class WolframAlpha:
         self.bot = bot
 
     @commands.command(aliases=['wa', 'wolframalpha', 'wolfram'])
-    async def wolframAlpha(self, *, query:str):
+    async def wolframAlpha(self, *, query: str):
         """
         Ask the Wolfram Alpha site anything.
         Uses the Short Answer API for the response.
@@ -21,9 +22,20 @@ class WolframAlpha:
         :return: Short Answer
         """
         url = "https://api.wolframalpha.com/v1/result"
+        link = "http://www.wolframalpha.com/input/?"
+
         payload = {'appid': wolfram_AppID, 'i': query}
+        link_payload = {'i': query}
+
+        link += urllib.parse.urlencode(link_payload)
+        print(link)
 
         with aiohttp.ClientSession() as session:
             async with session.get(url, params=payload) as resp:
                 print(resp.headers)
-                await self.bot.say(await resp.text())
+                desc = await resp.text()
+                embed = discord.Embed(title=query, description=desc, colour=discord.Colour.orange())
+                embed.set_footer(text='Wolfram|Alpha')
+                embed.set_author(name="Wolfram|Alpha", url=link,
+                                 icon_url="https://products.wolframalpha.com/waapi-headerfooter/wa-header-logo.svg")
+                await self.bot.say(embed=embed)
