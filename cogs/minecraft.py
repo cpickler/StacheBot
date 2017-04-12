74061ef4299ff9cdd72e4c100c85db0b54933040
cpickler <christian.pickler@gmail.com>
2017-04-12 00:54:51 -0700

    Status command is somewhat working, still need to handle exceptions

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -16,8 +16,40 @@ class Minecraft:
 
     @commands.command()
     async def status(self):
-        server = MinecraftServer.lookup(default_MC)
         embed = discord.Embed(title=default_MC)
+        server = MinecraftServer.lookup(default_MC)
         status = server.status()
+
+        # Add Green color for a server that's up
+        embed.colour = discord.Colour.green()
+
+        # Add a list of online players
+        if len(status.players.sample) >= 1:
+            online_players = ''
+            first = True
+            for player in status.players.sample:
+                if first is False:
+                    online_players += '\n'
+                online_players += player.name
+                first = False
+        else:
+            online_players = 'No one is online!'
+
+        embed.add_field(name="Players Online: {online}/{max}".format(online=status.players.online,
+                                                                    max=status.players.max),
+                        value=online_players)
+
+        # Remove formatting codes from the description
+        clean_description = ''
+        del_char = False
+        for i in status.description:
+            if del_char is True:
+                del_char = False
+            elif i != '§':
+                clean_description += i
+            else:
+                del_char = True
+
+        embed.description = clean_description
         await self.bot.say(embed=embed)
 
